007ba93e11dda06dc81837e2ee6b74f1d5da4bf2
Juan <105739344+contra3000@users.noreply.github.com>
2022-08-12 13:01:58 -0300

    Add files via upload

---
+++ CS/Ejercicios/04.Bucles/Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * WHILE
+Escribe una tabla de multiplicar del 1 al 10 para un número entero que recibe por consola. 
+Es decir, un programa que presente para el 1: 
+*1 x 1 = 1
+*1 x 2 = 2
+*…
+*1 x 10 = 10
+*/
+// Sumar los 100 primeros numeros enteros
+Console.WriteLine("Te mostraremos la tabla de multiplicar del numero que nos indiques: ");
+int j = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while (i <= 10)
+{
+    Console.WriteLine($"{j} * {i} = {j*i}");
+    i++;
+}
+Console.WriteLine("Adios!");
