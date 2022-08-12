228cee84477c8503cdbb25ab2f60244c3e639c37
Juan <105739344+contra3000@users.noreply.github.com>
2022-08-12 13:02:53 -0300

    Add files via upload

---
+++ CS/Ejercicios/04.Bucles/Ejercicio2/Ejercicio2/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * DO WHILE
+Escribe un programa que realice estos pasos:
+*Reciba al menos un número por consola
+*Determine si el número es positivo o negativo
+*Presente un contador para cada tipo (positivo y negativo).
+Nota: el cero se puede abordar en una clase adicional ya que no es ni positivo ni negativo. 
+Tienes completa libertad para elegir el formato de la salida.
+*/
+
+Console.WriteLine("Almacenemos 10 numeros enteros positivos o negativos.");
+int i = 0;
+var p = new List<int>();
+var n = new List<int>();
+do
+{
+    Console.WriteLine("Ingrese un número: ");
+    int j = Convert.ToInt32(Console.ReadLine());
+    if (j < 0)
+    {
+        Console.WriteLine("Número negativo OK");
+        n.Add(j);
+    }
+    else if (j > 0)
+    {
+        Console.WriteLine("Número positivo OK");
+        p.Add(j);
+    }
+    else
+    {
+        Console.WriteLine("Este numero es cero");
+    }
+    Console.WriteLine($"{p.Count} números positivos almacenados");
+    Console.WriteLine($"{n.Count} números negativos almacenados");
+}
+while ((p.Count < 10) & (n.Count <10));
+
+Console.WriteLine("Adios!");
