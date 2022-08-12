611fff0f121783abf340efd4e8d02ab84b854ac1
Juan <105739344+contra3000@users.noreply.github.com>
2022-08-12 13:05:08 -0300

    Add files via upload

---
+++ CS/Ejercicios/05.Bucles2/Ejercicio1/Program.cs
@@ -0,0 +1,59 @@
+﻿/*
+ IF
+Escribe un programa que:
+* Pida datos a un cliente: Nombre, email, cupón
+* Determine si un cliente tiene un cupon descuento
+* Muestre un precio rebajado en función del descuento
+* Muestre el precio de un producto sin descuento si no hay cupón
+Nota: puedes poner un precio fijo de un producto o uno variable. 
+ */
+
+Console.WriteLine("Ingrese nombre completo: ");
+string a001name = Console.ReadLine();
+Console.WriteLine("Ingrese e-mail: ");
+string a001mail = Console.ReadLine();
+Console.WriteLine("Dispone de descuento?(true o false) ");
+bool a001cupon = Convert.ToBoolean(Console.ReadLine());
+Cliente a001 = new Cliente(a001name, a001mail, a001cupon);
+
+const double sanFrancisco = 15.00d;
+const double mojito = 15.00d;
+const double tequilaSunrise = 19.00d;
+const double cubaLibre = 9.00d;
+const double espressoMartini = 12.00d;
+
+if (a001cupon)
+{
+    Console.WriteLine($"Bienvenido {a001name}");
+    Console.WriteLine("Precios con su descuento del 25%!!");
+    Console.WriteLine($"San Francisco: {sanFrancisco * 0.75}usd");
+    Console.WriteLine($"Mojito: {mojito * 0.75}usd");
+    Console.WriteLine($"Tequila Sunrise: {tequilaSunrise * 0.75}usd");
+    Console.WriteLine($"Cuba Libre: {cubaLibre * 0.75}usd");
+    Console.WriteLine($"Espresso Martini: {espressoMartini * 0.75}usd");
+}
+else
+{
+    Console.WriteLine($"Bienvenido {a001name}");
+    Console.WriteLine("Precios imperdibles!!");
+    Console.WriteLine($"San Francisco: {sanFrancisco}usd");
+    Console.WriteLine($"Mojito: {mojito}usd");
+    Console.WriteLine($"Tequila Sunrise: {tequilaSunrise}usd");
+    Console.WriteLine($"Cuba Libre: {cubaLibre}usd");
+    Console.WriteLine($"Espresso Martini: {espressoMartini}usd");
+}
+
+public struct Cliente
+{
+    public Cliente(string name, string mail, bool cupon)
+    {
+        Name = name;
+        Mail = mail;
+        Cupon = cupon;
+    }
+    public string Name { get; set; }
+    public string Mail { get; set;  }
+    public bool Cupon { get; set; }
+
+    public override string ToString() => $"({Name}, {Mail}, {Cupon})";
+}
---
+++ CS/Ejercicios/05.Bucles2/Ejercicio2/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+SWITCH
+* Haz una lista de lenguajes de programación, por ejemplo: C#, Java, C++.
+* Presenta la lista en consola y pide que el usuario seleccione el lenguaje mediante 1, 2, 3 o a, b, c. 
+* Presenta el resultado en consola.
+Nota: puedes añadir al resultado el “Hola, mundo” para el caso de C#.
+*/
+
+List<string> leng = new List<string>()
+{
+    "Java", "Python", "C++", "C#", "Javascript"
+};
+Console.WriteLine("Elije un lenguaje tipeando el numero correspondiente");
+for (int i = 1; i <= leng.Count; i++)
+{
+    Console.WriteLine($"{i}. {leng[i-1]}");
+}
+int choice = Convert.ToInt32(Console.ReadLine());
+switch(choice){
+    case 1:
+        Console.WriteLine("Usted seleccionó 'Java'");
+        break;
+    case 2:
+        Console.WriteLine("Usted seleccionó 'Python'");
+        break;
+    case 3:
+        Console.WriteLine("Usted seleccionó 'C++'");
+        break;
+    case 4:
+        Console.WriteLine("Usted seleccionó 'C#'");
+        Console.WriteLine("Hola, mundo");
+        break;
+    case 5:
+        Console.WriteLine("Usted seleccionó 'Javascript'");
+        break;
+    default:
+        Console.WriteLine("Ese no es un número válido");
+        break;
+}
