b053b17a5d40bf80e1656b917be74a3a915c1d6e
Juan <105739344+contra3000@users.noreply.github.com>
2022-10-03 15:31:31 -0300

    Delete Spring/Ejercicios/Ejercicio123-HelloController directory

--- Spring/Ejercicios/Ejercicio123-HelloController/src/main/java/com/example/ob03restdatajpa/repository/LaptopRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ob03restdatajpa.repository;
-
-import com.example.ob03restdatajpa.entities.Laptop;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LaptopRepository extends JpaRepository<Laptop, Long> {
-}
