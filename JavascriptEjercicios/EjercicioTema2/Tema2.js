cf22ab83885775ebeb99241553934d788915fa25
Juan <105739344+contra3000@users.noreply.github.com>
2023-03-17 11:45:39 -0300

    Add files via upload

---
+++ JavascriptEjercicios/EjercicioTema2/Tema2.js
@@ -0,0 +1,21 @@
+/*
+Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+- Tu nombre (string)
+- Tu edad (number)
+- ¿Eres desarrollador? (boolean)
+- Tu fecha de nacimiento (Date)
+- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+*/
+
+const contra3000 = [
+    "Juan Manuel Contreras", 
+    32, 
+    true, 
+    new Date(1990, 8, 9), 
+    favoriteBook = {
+        titulo: "Harry Potter", 
+        autor: "J. K. Rowling", 
+        fecha: new Date(1997, 7, 1), 
+        url: "https://es.wikipedia.org/wiki/Harry_Potter_y_la_piedra_filosofal"
+    }
+]
