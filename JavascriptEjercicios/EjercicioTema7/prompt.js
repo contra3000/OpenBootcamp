46714afbc8f948a63457d0ed78f117290da99202
Juan <105739344+contra3000@users.noreply.github.com>
2023-03-25 16:01:23 -0300

    Add files via upload

---
+++ JavascriptEjercicios/EjercicioTema7/prompt.js
@@ -0,0 +1,9 @@
+/*
+- Abre una nueva ventana "about:blank" en Google Chrome
+- Abre la consola de desarrollador de Google Chrome (F12)
+- Pregunta al usuario cuál es su edad y almacénala en una variable llamada edad
+*/
+
+const edad = prompt("¿Cuál es tu edad?");
+const num_edad = edad;
+console.log('num_edad :>> ', num_edad);
