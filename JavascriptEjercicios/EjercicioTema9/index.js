a2863c5019787c2c1507213aeb37d31662a1bdc9
Juan <105739344+contra3000@users.noreply.github.com>
2023-03-27 20:36:18 -0300

    Winston
    Explicación directorio winston

--- JavascriptEjercicios/EjercicioTema9/index.js
+++ JavascriptEjercicios/EjercicioTema9/index.js
@@ -3,6 +3,7 @@ Crea un nuevo proyecto de Node
 - Instala la dependencia Winston
 - En el archivo index.js crea una función que devuelva un error con un mensaje personalizado
 - Registra el error en un archivo a través de un try...catch
+(el directorio de la dependencia Winston no fue subido para evitar el sobreuso de almacenamiento en esta carpeta)
 */
 
 function cuadrado(numero){
