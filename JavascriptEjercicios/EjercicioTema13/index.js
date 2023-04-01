02b7e97f35374c8502a8e8add8200f3da4fce434
Juan <105739344+contra3000@users.noreply.github.com>
2023-04-01 14:43:18 -0300

    Ejercicio 13
    Se carga archivo obviando carpeta node_modules, a los fines de ahorrar recursos.

---
+++ JavascriptEjercicios/EjercicioTema13/index.js
@@ -0,0 +1,35 @@
+// Strings
+let str = "Hola soy un texto"
+let strSingle = "Hola soy un texto con comillas simples"
+
+console.log("str :>> ", str)
+console.log("strSingle :>> ", strSingle)
+
+let string_comillas = "El otro dia me dijeron \"Que barbaridad\""
+let string_comillasDobles = "El otro dia me dijeron 'Que barbaridad'"
+let string_comillasSimples = "El otro dia me dijeron \"Que barbaridad\""
+
+console.log("string_comillas :>> ", string_comillas)
+console.log("string_comillas :>> ", string_comillasDobles)
+console.log("string_comillas :>> ", string_comillasSimples)
+
+// Comillas invertidas
+
+let str_backticks = "hola soy un string con comillas invertidas"
+
+console.log("str_backticks :>> ", str_backticks)
+
+let nombre = "Gorka"
+let saludo = `Hola ${nombre} bienvenido!`
+
+console.log("saludo :>> ", saludo)
+
+// plantilla html
+let plantilla = `
+<html>
+    <h1>Pagina web de ${nombre}</h1>
+    <p>Este es un parrafo</p>
+</html>
+`
+console.log("plantilla :>> ", plantilla)
+
